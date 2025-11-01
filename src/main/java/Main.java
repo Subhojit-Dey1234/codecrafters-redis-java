226c8cae6a04e51f17e4813cd509276d5ec6c4cc
Subhojit Dey <subhojit2000dey@gmail.com>
2025-11-01 07:12:56 +0530

    Codecrafters Stage 4 - Using threadpool

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,53 +4,38 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Logs from your program will appear here!");
         int port = 6379;
-        boolean listening = true;
-
-        ExecutorService executor = Executors.newVirtualThreadPerTaskExecutor();
-
-        try (ServerSocket serverSocket = new ServerSocket(port)) {
+        try {
+            ServerSocket serverSocket = new ServerSocket(port);
             serverSocket.setReuseAddress(true);
-
-            while (listening) {
-                Socket clientSocket = serverSocket.accept();
-                System.out.println("New client connected");
-                executor.submit(()-> handleClient(clientSocket));
+            while (true) {
+                Socket finalSocket = serverSocket.accept();
+                new Thread(() -> handleRequest(finalSocket)).start();
             }
-
         } catch (IOException e) {
             System.out.println("IOException: " + e.getMessage());
-            System.exit(-1);
         }
     }
 
-    static void handleClient(Socket clientSocket) {
-        try (
-                clientSocket; // automatically closes socket at the end
-                OutputStream outputStream = clientSocket.getOutputStream();
-                BufferedReader in = new BufferedReader(
-                        new InputStreamReader(clientSocket.getInputStream()))
-        ) {
+    static void handleRequest(Socket clientSocket) {
+        try(clientSocket;
+            OutputStream outputStream = clientSocket.getOutputStream();
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(clientSocket.getInputStream())
+            )){
 
             while (true) {
-                // hacky way to read input; will change
-                if (in.readLine() == null) {
-                    break;
-                }
-                in.readLine();
-                String line = in.readLine();
-                System.out.println("Last line: " + line);
+                if(in.readLine() == null) break;
                 outputStream.write("+PONG\r\n".getBytes());
             }
-
-
         } catch (IOException e) {
-            System.out.println("IOException: " + e.getMessage());
+            throw new RuntimeException(e);
         }
     }
+
+
 }
