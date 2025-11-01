a3b1a122323a538631b4f1b38a70ae7c3e0b0147
Subhojit Dey <subhojit2000dey@gmail.com>
2025-11-01 11:31:48 +0530

    Codecrafters Stage 5 | Testing the redis parser

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,50 +1,61 @@
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.nio.charset.StandardCharsets;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class Main {
-    static void main(String[] args) {
+    public static void main(String[] args){
+        // You can use print statements as follows for debugging, they'll be visible when running tests.
         System.out.println("Logs from your program will appear here!");
+
+        //  Uncomment this block to pass the first stage
+        ServerSocket serverSocket = null;
         int port = 6379;
         try {
-            ServerSocket serverSocket = new ServerSocket(port);
+            serverSocket = new ServerSocket(port);
+            // Since the tester restarts your program quite often, setting SO_REUSEADDR
+            // ensures that we don't run into 'Address already in use' errors
             serverSocket.setReuseAddress(true);
-            while (true) {
-                Socket finalSocket = serverSocket.accept();
-                new Thread(() -> handleRequest(finalSocket)).start();
+            // Wait for connection from client.
+            while(true){
+                Socket clientSocket = serverSocket.accept();
+                Thread clientThread = new Thread(() ->{
+                    try{
+                        processBuffer(clientSocket);
+                    } catch(Exception e){
+                        System.out.println("Exception: " + e.getMessage());
+                    }
+                });
+                clientThread.start();
             }
-        } catch (IOException ignored) {
-            System.exit(-1);
+
+        } catch (IOException e) {
+            System.out.println("IOException: " + e.getMessage());
         }
     }
 
-    static void handleRequest(Socket clientSocket) {
-        try(clientSocket;
-            OutputStream outputStream = clientSocket.getOutputStream();
-            BufferedReader in = new BufferedReader(
-                    new InputStreamReader(clientSocket.getInputStream(), StandardCharsets.UTF_8)
-            );){
+    private static void processBuffer(Socket clientSocket){
+        try (BufferedReader clientInput = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+             BufferedWriter clientOutput = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));){
             String content;
-            while ((content = in.readLine()) != null) {
-                if(in.readLine() == null) break;
+
+            while((content = clientInput.readLine()) != null){
                 if(content.equalsIgnoreCase("ping")){
-                    outputStream.write("+PONG\r\n".getBytes());
-                    outputStream.flush();
+                    clientOutput.write("+PONG\r\n");
+                    clientOutput.flush();
                 } else if(content.equalsIgnoreCase("echo")){
-                    String numBytes = in.readLine();
-                    outputStream.write((numBytes + "\r\n" + in.readLine() + "\r\n").getBytes());
-                    outputStream.flush();
+                    String numBytes = clientInput.readLine();
+                    clientOutput.write(numBytes + "\r\n" + clientInput.readLine() + "\r\n");
+                    clientOutput.flush();
                 }
             }
-        }
-        catch (IOException ignored) {
-            System.out.println(ignored.getMessage());
+        } catch (IOException e){
+            throw new RuntimeException(e);
         }
     }
-
-
 }
