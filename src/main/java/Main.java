9bfca50a47424d4bf7b3813627fdc15da1d45718
Subhojit Dey <subhojit2000dey@gmail.com>
2025-11-01 06:48:27 +0530

    git commit -am "Codecrafters Stage 4"

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,36 +1,52 @@
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
 
 public class Main {
-  public static void main(String[] args){
-    System.out.println("Logs from your program will appear here!");
+    public static void main(String[] args) {
+        System.out.println("Logs from your program will appear here!");
         ServerSocket serverSocket = null;
         Socket clientSocket = null;
         int port = 6379;
         try {
-          serverSocket = new ServerSocket(port);
-          serverSocket.setReuseAddress(true);
-          clientSocket = serverSocket.accept();
+            serverSocket = new ServerSocket(port);
+            serverSocket.setReuseAddress(true);
+            clientSocket = serverSocket.accept();
+            ExecutorService executorService = Executors.newFixedThreadPool(2);
 
-          while (true){
-              byte[] buffer = new byte[1024];
-              int read = clientSocket.getInputStream().read(buffer);
-              String message = new String(buffer, 0, read).trim();
-              System.out.println("Received: " + message);
-              clientSocket.getOutputStream().write("+PONG\r\n".getBytes());
-          }
+            while (true) {
+                Socket finalClientSocket = clientSocket;
+                executorService.submit(() -> {
+                    byte[] buffer = new byte[1024];
+                    int read = 0;
+                    try {
+                        read = finalClientSocket.getInputStream().read(buffer);
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                    String message = new String(buffer, 0, read).trim();
+                    System.out.println("Received: " + message);
+                    try {
+                        finalClientSocket.getOutputStream().write("+PONG\r\n".getBytes());
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                });
+            }
 
         } catch (IOException e) {
-          System.out.println("IOException: " + e.getMessage());
-        } finally {
-          try {
-            if (clientSocket != null) {
-              clientSocket.close();
-            }
-          } catch (IOException e) {
             System.out.println("IOException: " + e.getMessage());
-          }
+        } finally {
+            try {
+                if (clientSocket != null) {
+                    clientSocket.close();
+                }
+            } catch (IOException e) {
+                System.out.println("IOException: " + e.getMessage());
+            }
         }
-  }
+    }
 }
